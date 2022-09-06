a59d8ff3e450cc71fd3237e442d219e5f894bb27
아기코쟁이 <90755590+haansohee@users.noreply.github.com>
2022-09-06 19:01:34 +0900

    Update

--- Week2/Week2/score1.c
+++
@@ -1,9 +0,0 @@
-//
-//  main.c
-//  Week2
-//
-//  Created by 한소희 on 2022/09/06.
-//
-
-#include <stdio.h>
-
--- Week2/Week2/scores4.c
+++
@@ -1,8 +0,0 @@
-//
-//  scores4.c
-//  Week2
-//
-//  Created by 한소희 on 2022/09/06.
-//
-
-#include <stdio.h>
